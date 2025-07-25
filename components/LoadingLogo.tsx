5abfdd8aa0e7d8ea377c1c6214afca727c5524a4
Cristobal <FHCristobal@hotmail.com>
2025-07-25 17:51:18 -0400

    fix: change loading text size on mobile

--- components/LoadingLogo.tsx
+++ components/LoadingLogo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const LoadingLogo = () => {
   return (
     <div className="fixed bg-gray-900 inset-0 flex justify-center items-center slide-out z-20">
-      <span className="text-9xl font-extrabold loader" anim-text="CRISTOBAL">CRISTOBAL</span>
+      <span className="text-5xl tracking-widest md:text-9xl font-extrabold loader" anim-text="CRISTOBAL">CRISTOBAL</span>
     </div>
   );
 };
