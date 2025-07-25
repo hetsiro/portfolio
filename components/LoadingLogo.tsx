b1167b32af874323d404b08e79cca3543f70bde1
Cristobal <FHCristobal@hotmail.com>
2025-07-25 17:45:50 -0400

    refactor: clean unused components and variables

---
+++ components/LoadingLogo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LoadingLogo = () => {
+  return (
+    <div className="fixed bg-gray-900 inset-0 flex justify-center items-center slide-out z-20">
+      <span className="text-9xl font-extrabold loader" anim-text="CRISTOBAL">CRISTOBAL</span>
+    </div>
+  );
+};
